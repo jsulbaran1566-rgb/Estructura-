713b3f7fb6c518e7521912a1a21b1470e218d02c
jsulbaran1566-rgb <jsulbaran1566@gmail.com>
2026-06-16 16:54:38 -0500

    Delete scr/Excepciones/excepciones.py

--- scr/Excepciones/excepciones.py
+++
@@ -1,8 +0,0 @@
-class ErrorUsuarioNoExiste(Exception):
-    def __init__(self, id):
-        self.mensaje = f"No existe un usuario con el id {id}"
-
-
-class ErrorStockInsuficiente(Exception):
-    def __init__(self, producto, pedido, disponible):
-        self.mensaje = f"No hay suficiente stock de '{producto}'. Pedido: {pedido}, Disponible: {disponible}"
